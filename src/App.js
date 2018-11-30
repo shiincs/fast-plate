4c134a6175289fbec8dabac8b342a04fac64e5af
Jo Minji <mingi2687@gamil.com>
2018-11-30 19:32:16 +0900

    헤더 완성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Layout from './components/Layout';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return <Layout />;
   }
 }
 
---
+++ src/components/HeaderView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+export default class HeaderView extends Component {
+  render() {
+    return (
+      <header className="Header">
+        <h1 className="Logo">MANGO PLATE</h1>
+        <nav className="Nav">
+          <ul className="__list">
+            <li className="__item">EAT DEAL</li>
+            <li className="__item">맛집 리스트</li>
+            <li className="__item">망고 스토리</li>
+            <button className="Myprofile">프로필</button>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
