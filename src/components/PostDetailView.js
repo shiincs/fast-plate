16dffcf9af215a22fa667ced89249039c75b02c1
Jo Minji <mingi2687@gamil.com>
2018-12-04 11:43:35 +0900

    PostDetailView 맵 추가하고 레이아웃 수정

--- src/components/PostDetailView.js
+++ src/components/PostDetailView.js
@@ -23,34 +23,37 @@ export default class PostDetailView extends Component {
     return (
       <React.Fragment>
         <div className="photo-list">이미지 캐러셀 들어갈 자리</div>
-        <div className="restaurant-detail">
-          <header>
-            <div className="titleWrap">
-              <h1 className="title">{restaurants.name}</h1>
-              <span className="rate" />
+        <div className="detail-inner">
+          <div className="restaurant-detail">
+            <header>
+              <div className="titleWrap">
+                <h1 className="title">{restaurants.name}</h1>
+                <span className="rate" />
+              </div>
+              <div className="status">
+                <span className="hit">{restaurants.view_num}</span>
+                <span className="review">{restaurants.review_num}</span>
+                <span className="favorite">{restaurants.want_num}</span>
+              </div>
+            </header>
+            <div>
+              <dl className="detail-list">
+                <dt>주소</dt>
+                <dd> {restaurants.address_detail}</dd>
+                <dt>전화번호</dt>
+                <dd>{restaurants.phone_num}</dd>
+                <dt>음식 종류</dt>
+                <dd>{restaurants.food_type}</dd>
+                <dt>가격대</dt>
+                <dd>{restaurants.price_level}</dd>
+                <dt>주차</dt>
+                <dd>{restaurants.parking}</dd>
+                <dt>영업시간</dt>
+                <dd>{restaurants.Business_hour}</dd>
+              </dl>
             </div>
-            <div className="status">
-              <span className="hit">{restaurants.view_num}</span>
-              <span className="review">{restaurants.review_num}</span>
-              <span className="favorite">{restaurants.want_num}</span>
-            </div>
-          </header>
-          <div>
-            <dl className="detail-list">
-              <dt>주소</dt>
-              <dd> {restaurants.address_detail}</dd>
-              <dt>전화번호</dt>
-              <dd>{restaurants.phone_num}</dd>
-              <dt>음식 종류</dt>
-              <dd>{restaurants.food_type}</dd>
-              <dt>가격대</dt>
-              <dd>{restaurants.price_level}</dd>
-              <dt>주차</dt>
-              <dd>{restaurants.parking}</dd>
-              <dt>영업시간</dt>
-              <dd>{restaurants.Business_hour}</dd>
-            </dl>
           </div>
+          <div className="map">지도</div>
         </div>
       </React.Fragment>
     );
