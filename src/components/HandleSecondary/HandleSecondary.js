481519167ebc762c8fec1272243210f90f28ef32
goongoguma <wogus7an@gmail.com>
2018-12-04 16:58:30 +0900

    section assortment

--- src/components/HandleSecondary/HandleSecondary.js
+++ src/components/HandleSecondary/HandleSecondary.js
@@ -7,6 +7,9 @@ import food3 from './SecondaryImg/food3.jpg';
 import food4 from './SecondaryImg/food4.jpeg';
 import food5 from './SecondaryImg/food5.jpg';
 import food6 from './SecondaryImg/food6.jpg';
+import food7 from './SecondaryImg/food7.jpg';
+import food8 from './SecondaryImg/food8.jpg';
+import food9 from './SecondaryImg/food9.jpg';
 
 const cx = classNames.bind(styles);
 
@@ -18,37 +21,104 @@ export default class HandleSecondary extends Component {
         {
           img: food1,
           text: '곱창전골 맛집 베스트',
+          keyword: '성수',
         },
         {
           img: food2,
           text: '이주의 EAT딜 베스트',
+          keyword: '성수',
         },
         {
           img: food3,
           text: '성북구 양식 맛집 베스트',
+          keyword: '성수',
         },
         {
           img: food4,
           text: '명동 맛집 베스트',
+          keyword: '명동',
         },
         {
           img: food5,
           text: '펍 베스트',
+          keyword: '명동',
         },
         {
           img: food6,
           text: '오믈렛 맛집 베스트',
+          keyword: '명동',
+        },
+        {
+          img: food7,
+          text: '들깨 칼국수 맛집 베스트',
+          keyword: '강남',
+        },
+        {
+          img: food8,
+          text: '생선구이 맛집 베스트',
+          keyword: '강남',
+        },
+        {
+          img: food9,
+          text: '비빔밥 맛집 베스트',
+          keyword: '강남',
         },
       ],
+      dummySlice: [],
     };
   }
 
-  render() {
+  componentDidMount() {
+    this.all();
+  }
+
+  all() {
     const { dummyImgTextArr } = this.state;
+    this.setState({
+      dummySlice: dummyImgTextArr,
+    });
+  }
+
+  sungsu() {
+    const { dummyImgTextArr } = this.state;
+    this.setState({
+      dummySlice: dummyImgTextArr.filter(dummy => {
+        return dummy.keyword === '성수';
+      }),
+    });
+  }
+
+  myundong() {
+    const { dummyImgTextArr } = this.state;
+    this.setState({
+      dummySlice: dummyImgTextArr.filter(dummy => {
+        return dummy.keyword === '명동';
+      }),
+    });
+  }
+
+  gangnam() {
+    const { dummyImgTextArr } = this.state;
+    this.setState({
+      dummySlice: dummyImgTextArr.filter(dummy => {
+        return dummy.keyword === '강남';
+      }),
+    });
+  }
+
+  render() {
+    const { dummySlice } = this.state;
     return (
       <div className={cx('secondary')}>
         <h2>믿고 보는 맛집 리스트</h2>
-        {dummyImgTextArr.map((dummy, index) => (
+        <div className={cx('tags')}>
+          <button onClick={() => this.all()}>#전체</button>
+          <button onClick={() => this.sungsu()}>#성수</button>
+          <button onClick={() => this.myundong()}>#명동</button>
+          <button onClick={() => this.gangnam()}>#강남</button>
+        </div>
+        <div>''</div>
+        {dummySlice.map((dummy, index) => (
           <HandleList
             key={dummy[index]}
             listImg={dummy.img}
