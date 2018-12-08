4a2271268e67711db2c67bcf9c0cd193d67a7de3
goongoguma <wogus7an@gmail.com>
2018-12-08 12:23:09 +0900

    split review component into two parts - container and presentation

--- src/components/WritingReviewView.js
+++ src/components/WritingReviewView.js
@@ -7,108 +7,17 @@ const cx = classNames.bind(styles);
 // 코드의 양 줄이기
 
 export default class WritingReviewView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      goodOpen: false,
-      okOpen: false,
-      notGoodOpen: false,
-      imagePath: [
-        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_recommend_face.svg',
-
-        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_recommend_active_face.svg',
-
-        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_ok_face.svg',
-
-        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_ok_active_face.svg',
-
-        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_not_recommend_face.svg',
-
-        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_not_recommend_active_face.svg',
-      ],
-    };
-  }
-
-  allFontBlack() {
-    document.querySelector('.pickColor1').style.color = '#181818';
-    document.querySelector('.pickColor2').style.color = '#181818';
-    document.querySelector('.pickColor3').style.color = '#181818';
-  }
-
-  toggleGoodOpen() {
-    this.setState(prevState => ({
-      goodOpen: !prevState.goodOpen,
-      okOpen: false,
-      notGoodOpen: false,
-    }));
-    if (this.state.goodOpen) {
-      this.allFontBlack();
-    } else {
-      document.querySelector('.pickColor1').style.color = '#ff792a';
-      document.querySelector('.pickColor2').style.color = '#181818';
-      document.querySelector('.pickColor3').style.color = '#181818';
-    }
-  }
-
-  toggleOkOpen() {
-    this.setState(prevState => ({
-      okOpen: !prevState.okOpen,
-      goodOpen: false,
-      notGoodOpen: false,
-    }));
-
-    if (this.state.okOpen) {
-      this.allFontBlack();
-    } else {
-      document.querySelector('.pickColor2').style.color = '#ff792a';
-      document.querySelector('.pickColor1').style.color = '#181818';
-      document.querySelector('.pickColor3').style.color = '#181818';
-    }
-  }
-
-  toggleNotGoodOpen() {
-    this.setState(prevState => ({
-      notGoodOpen: !prevState.notGoodOpen,
-      okOpen: false,
-      goodOpen: false,
-    }));
-    if (this.state.notGoodOpen) {
-      this.allFontBlack();
-    } else {
-      document.querySelector('.pickColor3').style.color = '#ff792a';
-      document.querySelector('.pickColor1').style.color = '#181818';
-      document.querySelector('.pickColor2').style.color = '#181818';
-    }
-  }
-
-  goodImageName() {
-    const { goodOpen, imagePath } = this.state;
-    if (goodOpen) {
-      return imagePath[1]; // active
-    } else {
-      return imagePath[0]; // normal
-    }
-  }
-
-  okImageName() {
-    const { okOpen, imagePath } = this.state;
-    if (okOpen) {
-      return imagePath[3]; // active
-    } else {
-      return imagePath[2]; // normal
-    }
-  }
-
-  notGoodImageName() {
-    const { notGoodOpen, imagePath } = this.state;
-    if (notGoodOpen) {
-      return imagePath[5]; // active
-    } else {
-      return imagePath[4]; // normal
-    }
-  }
-
   render() {
+    const {
+      goodOpen,
+      okOpen,
+      notGoodOpen,
+      imagePath,
+      toggleGoodOpen,
+      toggleOkOpen,
+      toggleNotGoodOpen,
+    } = this.props;
+
     return (
       <React.Fragment>
         <section className={cx('WritingReviewPage')}>
@@ -123,25 +32,25 @@ export default class WritingReviewView extends Component {
               <ul className={cx('RestaurantRecommendPicker__List')}>
                 <li className={cx('RestaurantRecommendPicker__Item')}>
                   <img
-                    src={this.goodImageName()}
+                    src={goodOpen ? imagePath[1] : imagePath[0]}
                     alt="좋아요 버튼"
-                    onClick={() => this.toggleGoodOpen()}
+                    onClick={toggleGoodOpen}
                   />
                   <p className={cx('pickColor1')}>맛있다</p>
                 </li>
                 <li className={cx('RestaurantRecommendPicker__Item')}>
                   <img
-                    src={this.okImageName()}
+                    src={okOpen ? imagePath[3] : imagePath[2]}
                     alt="괜찮다 버튼"
-                    onClick={() => this.toggleOkOpen()}
+                    onClick={toggleOkOpen}
                   />
                   <p className={cx('pickColor2')}>괜찮다</p>
                 </li>
                 <li className={cx('RestaurantRecommendPicker__Item')}>
                   <img
-                    src={this.notGoodImageName()}
+                    src={notGoodOpen ? imagePath[5] : imagePath[4]}
                     alt="별로 버튼"
-                    onClick={() => this.toggleNotGoodOpen()}
+                    onClick={toggleNotGoodOpen}
                   />
                   <p className={cx('pickColor3')}>별로</p>
                 </li>
--- src/containers/WritingReview.js
+++ src/containers/WritingReview.js
@@ -4,14 +4,86 @@ export default class WritingReview extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      reviewScore: 0,
+      goodOpen: false,
+      okOpen: false,
+      notGoodOpen: false,
+      imagePath: [
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_recommend_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_recommend_active_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_ok_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_ok_active_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_not_recommend_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_not_recommend_active_face.svg',
+      ],
     };
   }
+
+  allFontBlack = () => {
+    document.querySelector('.pickColor1').style.color = '#181818';
+    document.querySelector('.pickColor2').style.color = '#181818';
+    document.querySelector('.pickColor3').style.color = '#181818';
+  };
+
+  toggleGoodOpen = () => {
+    this.setState(prevState => ({
+      goodOpen: !prevState.goodOpen,
+      okOpen: false,
+      notGoodOpen: false,
+    }));
+    if (this.state.goodOpen) {
+      this.allFontBlack();
+    } else {
+      document.querySelector('.pickColor1').style.color = '#ff792a';
+      document.querySelector('.pickColor2').style.color = '#181818';
+      document.querySelector('.pickColor3').style.color = '#181818';
+    }
+  };
+
+  toggleOkOpen = () => {
+    this.setState(prevState => ({
+      okOpen: !prevState.okOpen,
+      goodOpen: false,
+      notGoodOpen: false,
+    }));
+
+    if (this.state.okOpen) {
+      this.allFontBlack();
+    } else {
+      document.querySelector('.pickColor2').style.color = '#ff792a';
+      document.querySelector('.pickColor1').style.color = '#181818';
+      document.querySelector('.pickColor3').style.color = '#181818';
+    }
+  };
+
+  toggleNotGoodOpen = () => {
+    this.setState(prevState => ({
+      notGoodOpen: !prevState.notGoodOpen,
+      okOpen: false,
+      goodOpen: false,
+    }));
+    if (this.state.notGoodOpen) {
+      this.allFontBlack();
+    } else {
+      document.querySelector('.pickColor3').style.color = '#ff792a';
+      document.querySelector('.pickColor1').style.color = '#181818';
+      document.querySelector('.pickColor2').style.color = '#181818';
+    }
+  };
+
   render() {
-    const { reviewScore } = this.state;
     return (
       <React.Fragment>
-        <WritingReviewView reviewScore={reviewScore} />
+        <WritingReviewView
+          {...this.state}
+          toggleGoodOpen={this.toggleGoodOpen}
+          toggleOkOpen={this.toggleOkOpen}
+          toggleNotGoodOpen={this.toggleNotGoodOpen}
+        />
       </React.Fragment>
     );
   }
