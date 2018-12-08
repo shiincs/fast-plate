c9987473af48278158624939ba56e8b99e9fb2a0
goongoguma <wogus7an@gmail.com>
2018-12-08 10:00:10 +0900

    making WritingReviewView component neat

--- src/components/WritingReviewView.js
+++ src/components/WritingReviewView.js
@@ -6,21 +6,6 @@ const cx = classNames.bind(styles);
 // 컴포넌트 나누기 (page, container, presentational)
 // 코드의 양 줄이기
 
-const imagePath = {
-  goodOff:
-    'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_recommend_face.svg',
-  goodOn:
-    'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_recommend_active_face.svg',
-  okOff:
-    'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_ok_face.svg',
-  okOn:
-    'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_ok_active_face.svg',
-  notGoodOff:
-    'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_not_recommend_face.svg',
-  notGoodOn:
-    'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_not_recommend_active_face.svg',
-};
-
 export default class WritingReviewView extends Component {
   constructor(props) {
     super(props);
@@ -28,82 +13,102 @@ export default class WritingReviewView extends Component {
       goodOpen: false,
       okOpen: false,
       notGoodOpen: false,
+      imagePath: [
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_recommend_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_recommend_active_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_ok_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_ok_active_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_not_recommend_face.svg',
+
+        'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/restaurant_not_recommend_active_face.svg',
+      ],
     };
   }
 
+  allFontBlack() {
+    document.querySelector('.pickColor1').style.color = '#181818';
+    document.querySelector('.pickColor2').style.color = '#181818';
+    document.querySelector('.pickColor3').style.color = '#181818';
+  }
+
   toggleGoodOpen() {
-    const { reviewScore } = this.props;
     this.setState(prevState => ({
       goodOpen: !prevState.goodOpen,
       okOpen: false,
       notGoodOpen: false,
     }));
-    document.querySelector('.pickColor1').style.color = '#ff792a';
-    document.querySelector('.pickColor2').style.color = '#181818';
-    document.querySelector('.pickColor3').style.color = '#181818';
     if (this.state.goodOpen) {
-      console.log(reviewScore);
-      document.querySelector('.pickColor1').style.color = '#181818';
+      this.allFontBlack();
     } else {
-      this.setState({
-        reviewScore: reviewScore + 5,
-      });
-      console.log(this.state.reviewScore);
+      document.querySelector('.pickColor1').style.color = '#ff792a';
+      document.querySelector('.pickColor2').style.color = '#181818';
+      document.querySelector('.pickColor3').style.color = '#181818';
     }
   }
 
   toggleOkOpen() {
-    const { reviewScore } = this.props;
     this.setState(prevState => ({
       okOpen: !prevState.okOpen,
       goodOpen: false,
       notGoodOpen: false,
     }));
-    document.querySelector('.pickColor2').style.color = '#ff792a';
-    document.querySelector('.pickColor1').style.color = '#181818';
-    document.querySelector('.pickColor3').style.color = '#181818';
+
     if (this.state.okOpen) {
-      console.log(reviewScore);
-      document.querySelector('.pickColor2').style.color = '#181818';
+      this.allFontBlack();
     } else {
-      this.setState({
-        reviewScore: reviewScore + 3,
-      });
-      console.log(this.state.reviewScore);
+      document.querySelector('.pickColor2').style.color = '#ff792a';
+      document.querySelector('.pickColor1').style.color = '#181818';
+      document.querySelector('.pickColor3').style.color = '#181818';
     }
   }
 
   toggleNotGoodOpen() {
-    const { reviewScore } = this.props;
     this.setState(prevState => ({
       notGoodOpen: !prevState.notGoodOpen,
       okOpen: false,
       goodOpen: false,
     }));
-    document.querySelector('.pickColor3').style.color = '#ff792a';
-    document.querySelector('.pickColor1').style.color = '#181818';
-    document.querySelector('.pickColor2').style.color = '#181818';
-
     if (this.state.notGoodOpen) {
-      console.log(reviewScore);
-      document.querySelector('.pickColor3').style.color = '#181818';
+      this.allFontBlack();
     } else {
-      this.setState({
-        reviewScore: reviewScore + 1,
-      });
-      console.log(this.state.reviewScore);
+      document.querySelector('.pickColor3').style.color = '#ff792a';
+      document.querySelector('.pickColor1').style.color = '#181818';
+      document.querySelector('.pickColor2').style.color = '#181818';
     }
   }
 
-  goodImageName = () => (this.state.goodOpen ? 'goodOn' : 'goodOff');
-  okImageName = () => (this.state.okOpen ? 'okOn' : 'okOff');
-  notGoodImageName = () =>
-    this.state.notGoodOpen ? 'notGoodOn' : 'notGoodOff';
+  goodImageName() {
+    const { goodOpen, imagePath } = this.state;
+    if (goodOpen) {
+      return imagePath[1]; // active
+    } else {
+      return imagePath[0]; // normal
+    }
+  }
+
+  okImageName() {
+    const { okOpen, imagePath } = this.state;
+    if (okOpen) {
+      return imagePath[3]; // active
+    } else {
+      return imagePath[2]; // normal
+    }
+  }
+
+  notGoodImageName() {
+    const { notGoodOpen, imagePath } = this.state;
+    if (notGoodOpen) {
+      return imagePath[5]; // active
+    } else {
+      return imagePath[4]; // normal
+    }
+  }
 
   render() {
-    const goodImage = this.goodImageName();
-    const okImage = this.okImageName();
-    const notGood = this.notGoodImageName();
     return (
       <React.Fragment>
         <section className={cx('WritingReviewPage')}>
@@ -118,7 +123,7 @@ export default class WritingReviewView extends Component {
               <ul className={cx('RestaurantRecommendPicker__List')}>
                 <li className={cx('RestaurantRecommendPicker__Item')}>
                   <img
-                    src={imagePath[goodImage]}
+                    src={this.goodImageName()}
                     alt="좋아요 버튼"
                     onClick={() => this.toggleGoodOpen()}
                   />
@@ -126,7 +131,7 @@ export default class WritingReviewView extends Component {
                 </li>
                 <li className={cx('RestaurantRecommendPicker__Item')}>
                   <img
-                    src={imagePath[okImage]}
+                    src={this.okImageName()}
                     alt="괜찮다 버튼"
                     onClick={() => this.toggleOkOpen()}
                   />
@@ -134,7 +139,7 @@ export default class WritingReviewView extends Component {
                 </li>
                 <li className={cx('RestaurantRecommendPicker__Item')}>
                   <img
-                    src={imagePath[notGood]}
+                    src={this.notGoodImageName()}
                     alt="별로 버튼"
                     onClick={() => this.toggleNotGoodOpen()}
                   />
