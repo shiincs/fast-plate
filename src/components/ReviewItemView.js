ef735e40c9f8f62809a7f604b15bede465f7049b
Jo Minji <mingi2687@gamil.com>
2018-12-12 17:07:49 +0900

    Modify: input review images

--- src/components/ReviewItemView.js
+++ src/components/ReviewItemView.js
@@ -10,10 +10,11 @@ export default class ReviewItemView extends Component {
     return (
       <>
         {container.map(r => (
+          // console.log(r.image)
           <div key={r.pk} className={cx('reveiw-content')}>
             <figure>
               <div className={cx('user-thumb')}>
-                <img src="" alt="" />
+                <img src={r.author.img_profile} alt="" />
               </div>
               <figcaption>{r.author.username}</figcaption>
             </figure>
@@ -21,9 +22,11 @@ export default class ReviewItemView extends Component {
               <span className={cx('date')}>2018-10-20</span>
               <div className={cx('review')}>{r.content}</div>
               <div className={cx('list-thumb-photos')}>
-                <button className={cx('button-thumb')} />
-                <button className={cx('button-thumb')} />
-                <button className={cx('button-thumb')} />
+                {r.postimage_posts.map(img => (
+                  <button className={cx('button-thumb')} key={img.pk}>
+                    <img src={img.image} alt="" />
+                  </button>
+                ))}
               </div>
             </div>
             <div className={cx('icon-rating')}>
