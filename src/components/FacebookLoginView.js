80af9c55720054a396e5ed57d2b2f575d4b7b32e
goongoguma <wogus7an@gmail.com>
2018-12-16 19:17:04 +0900

    facebook login

---
+++ src/components/FacebookLoginView.js
@@ -0,0 +1,50 @@
+import FacebookLogin from 'react-facebook-login';
+
+import React, { Component } from 'react';
+import HeaderView from './HeaderView';
+
+export default class FacebookLoginView extends Component {
+  state = {
+    isLoggedIn: false,
+    userID: '',
+    name: '',
+    email: '',
+    picture: '',
+  };
+
+  responseFacebook = response => {
+    console.log(response);
+    this.setState({
+      isLoggedIn: true,
+      userID: response.userID,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url,
+    });
+  };
+
+  componentClicked = () => console.log('componentClicked!');
+
+  render() {
+    let fbContent;
+
+    if (this.state.isLoggedIn) {
+      fbContent = (
+        <div>
+          <img src={this.state.picture} alt={this.state.name} />
+        </div>
+      );
+    } else {
+      fbContent = (
+        <FacebookLogin
+          appId="323220401853541"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      );
+    }
+    return <div>{fbContent}</div>;
+  }
+}
