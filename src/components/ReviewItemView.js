abc403356b4a0fc315acaa7521b39cad0ea2ef46
Jo Minji <mingi2687@gamil.com>
2018-12-18 12:30:42 +0900

    Modify: detailPage modal CSS

--- src/components/ReviewItemView.js
+++ src/components/ReviewItemView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ReviewItemView.module.scss';
 import withLoading from '../hoc/withLoading';
+import reviewUserImg from '../commonimgs/reviewUser.png';
 const cx = classNames.bind(styles);
 
 class ReviewItemView extends Component {
@@ -10,45 +11,54 @@ class ReviewItemView extends Component {
 
     return (
       <>
-        {container.map(r => (
-          // console.log(r.image)
-          <div key={r.pk} className={cx('reveiw-content')}>
-            <figure>
-              <div className={cx('user-thumb')}>
-                <img src={r.author.img_profile} alt="" />
+        {container.map(r => {
+          let imageSrc;
+          if (r.author.img_profile) {
+            imageSrc = r.author.img_profile;
+          } else {
+            imageSrc = reviewUserImg;
+          }
+
+          return (
+            // console.log(r.image)
+            <div key={r.pk} className={cx('reveiw-content')}>
+              <figure>
+                <div className={cx('user-thumb')}>
+                  <img src={imageSrc} alt="" />
+                </div>
+                <figcaption>{r.author.username}</figcaption>
+              </figure>
+              <div className={cx('review-wrapper')}>
+                <span className={cx('date')}>2018-10-20</span>
+                <div className={cx('review')}>{r.content}</div>
+                <div className={cx('list-thumb-photos')}>
+                  {r.postimage_posts.map(img => (
+                    <button className={cx('button-thumb')} key={img.pk}>
+                      <img src={img.image} alt={img.pk} />
+                    </button>
+                  ))}
+                </div>
               </div>
-              <figcaption>{r.author.username}</figcaption>
-            </figure>
-            <div className={cx('review-wrapper')}>
-              <span className={cx('date')}>2018-10-20</span>
-              <div className={cx('review')}>{r.content}</div>
-              <div className={cx('list-thumb-photos')}>
-                {r.postimage_posts.map(img => (
-                  <button className={cx('button-thumb')} key={img.pk}>
-                    <img src={img.image} alt="" />
-                  </button>
-                ))}
+              <div className={cx('icon-rating')}>
+                <strong
+                  className={cx(
+                    { bad: r.rate === 1 },
+                    { ok: r.rate === 3 },
+                    { good: r.rate === 5 }
+                  )}
+                >
+                  {r.rate === 1
+                    ? '별로'
+                    : r.rate === 3
+                    ? '괜찮다'
+                    : r.rate === 5
+                    ? '맛있다'
+                    : 'null'}
+                </strong>
               </div>
             </div>
-            <div className={cx('icon-rating')}>
-              <strong
-                className={cx(
-                  { bad: r.rate === 1 },
-                  { ok: r.rate === 3 },
-                  { good: r.rate === 5 }
-                )}
-              >
-                {r.rate === 1
-                  ? '별로'
-                  : r.rate === 3
-                  ? '괜찮다'
-                  : r.rate === 5
-                  ? '맛있다'
-                  : 'null'}
-              </strong>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </>
     );
   }
