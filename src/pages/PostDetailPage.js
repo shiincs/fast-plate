08323342ff6804eb7f7b9d6be8b6a0eb40e72316
Shin Changseon <skywing27@gmail.com>
2018-12-20 18:59:43 +0900

    refactor: remove unused variables

--- src/pages/PostDetailPage.js
+++ src/pages/PostDetailPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PostDetail from '../containers/PostDetail';
 import { withPage } from '../contexts/PageContext';
-import { WannagoProvider, withWannago } from '../contexts/WannagoContext';
 
 class PostDetailPage extends Component {
   componentDidMount() {
